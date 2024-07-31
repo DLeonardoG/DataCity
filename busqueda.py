6f91599b01d96c445adab8e817fc33598629de46
ErikSneyPlata <eplata391@gmail.com>
2024-07-31 12:35:09 -0500

    feat: :art: Se crea una nueva funcion para el uso del menu
    La funcion creada para buscar por paises de forma correcta, y ahorrando espacio en archivos.
    
    by @ErikSneyPlata

--- busqueda.py
+++ busqueda.py
@@ -37,3 +37,41 @@ def leer_ciudad_nombre():
         if continuar == "2": break
         else: clear_screen()
 
+# ----------------------------
+
+def leer_city_pais():
+    datos= funciones.cargar_datos()
+    clear_screen()
+    nombre =input("Ingrese el nombre del pais: ").lower()
+    for i in range(len(datos["ciudades"])):
+        if datos["ciudades"][i]["pais"] == nombre:
+            linea()
+            es()
+            # line()
+            print_("Nombre ciudad: ",datos["ciudades"][i]["nombre"].capitalize())
+            # line()
+            es()
+            # line()
+            print_("Codigo postal: ",datos["ciudades"][i]["codigo postal"].capitalize())
+            # line()
+            es()
+            # line()
+            print_("Poblacion : ",datos["ciudades"][i]["poblacion"])
+            # line()
+            es()
+            # line()
+            print_("Pais: ",datos["ciudades"][i]["pais"].capitalize())
+            # line()
+            es()
+            linea()
+            return
+    print_(f"La ciudad ",nombre," no existe...")
+    return
+
+def leer_ciudad_pais():
+    while True:
+        leer_city_pais()
+        continuar = very()
+        if continuar == "2": break
+        else: clear_screen()
+
