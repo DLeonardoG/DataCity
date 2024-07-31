ebc257e2e788a7d9015a20001c46418013d9c266
DLeonardoG <darkleonardog@gmail.com>
2024-07-31 11:53:04 -0500

    feat: :zap: Se realizo la funcion reespectiva de registrar datos
    Se creo la funcion adeciuada para la creacion de ciudades
    
    by @DLeonardoG

--- registro.py
+++ registro.py
@@ -1,12 +1,27 @@
 import json
 import funciones
+from funciones_segundarias import *
 
-def registro_ciudades():
-    while True:        
-        data = funciones.leer_crear_json()
-        print("REGISTRO DE CIUDAD")
-        ciudad = input("Ingrese el nombre de la ciudad: ")
-        
-            
-        
-            
+def crear_city():
+    datos = funciones.cargar_datos()
+    ciudades={}
+    clear_screen()
+    ciudades["nombre"]=input("Ingrese el nombre de la ciudad: ").lower()
+    ciudades["codigo postal"]=input("Ingrese el codigo postal: ").lower()
+    ciudades["poblacion"]=input("Ingrese poblacion de la ciudad: ").lower()
+    ciudades["pais"]=input("Ingrese el pais: ")
+    
+    datos["ciudades"].append(ciudades)
+    print(ciudades["nombre"]," ha sido registrada con éxito!")
+    funciones.guardar_datos(datos)
+    return
+
+def crear_ciudad():
+    while True:
+        crear_city()
+        continuar = very()
+        if continuar == "2": break
+        else: clear_screen()
+
+
+
