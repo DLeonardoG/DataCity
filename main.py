5ddcf670bc898a657da768ed4258dc38767e9f4d
ErikSneyPlata <eplata391@gmail.com>
2024-07-31 11:15:58 -0500

    feat: :art: Se crea el archivo py
    el archivo py de registro donde van las funciones de registro que usara el main
    
    by  @ErikSneyPlata

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+
+def main():
+    print("Bienvenido a Datacity")
+    print("1. registro de datos de ciudades")
+    print("2. Editar registros")
+    print("-----------------------------------------------")
+    print("3. mostrar registros")
+    print("4. mostrar registros por poblacion mayor")
+    print("5. mostrar registros por poblacion menor")
+    print("-----------------------------------------------")
+    print("6. busquedad por nombre")
+    print("7. busquedad por pais")
+    print("8. busquedad por codigo postal")
+    print("-----------------------------------------------")
+    print("9. salir")
