2d67ab63dd969774b96b0d96d4b895db55bcbda6
ErikSneyPlata <eplata391@gmail.com>
2024-07-31 12:51:46 -0500

    feat: :art: Se añade nueva funcionalidad para busqueda
    funcionalidad para busqueda por codigo postal para el menu
    
    by @ErikSneyPlata

--- busqueda.py
+++ busqueda.py
@@ -75,3 +75,43 @@ def leer_ciudad_pais():
         if continuar == "2": break
         else: clear_screen()
 
+# ---------------------------------------------
+
+def leer_city_cod():
+    datos= funciones.cargar_datos()
+    clear_screen()
+    nombre =input("Ingrese el codigo postal de la ciudad: ").lower()
+    for i in range(len(datos["ciudades"])):
+        if datos["ciudades"][i]["codigo postal"] == nombre:
+            linea()
+            es()
+            # line()
+            print_("Nombre ciudad: ",datos["ciudades"][i]["nombre"].capitalize())
+            # line()
+            es()
+            # line()
+            print_("Codigo postal: ",datos["ciudades"][i]["codigo postal"].capitalize())
+            # line()
+            es()
+            # line()
+            print_("Poblacion : ",datos["ciudades"][i]["poblacion"])
+            # line()
+            es()
+            # line()
+            print_("Pais: ",datos["ciudades"][i]["pais"].capitalize())
+            # line()
+            es()
+            linea()
+            return
+    print_(f"La ciudad ",nombre," no existe...")
+    return
+
+def leer_ciudad_cod():
+    while True:
+        leer_city_cod()
+        continuar = very()
+        if continuar == "2": break
+        else: clear_screen()
+
+
+
